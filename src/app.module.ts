6c50615e24cbe3ce2d481334ed9e188c713ed446
fevico <ajayifavour81@gmail.com>
2026-08-31 23:01:39 +0100

    course module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, NotificationModule],
+  imports: [PrismaModule, AuthModule, NotificationModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/course/course.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { CourseService } from './course.service';
+
+@Controller('course')
+export class CourseController {
+    constructor(private courseService: CourseService){}
+}
